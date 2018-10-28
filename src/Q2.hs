8d081ac9f1251e4ff8d4b16b020e0e541c04f696
Lim Chee Hau <ch33hau@gmail.com>
2018-10-28 16:45:11 +0100

    fix-01

--- src/Q2.hs
+++ src/Q2.hs
@@ -9,19 +9,19 @@ data Student = Student
     String String
     deriving (Show)
 
-xxx :: [Student]
-xxx = [Student "A" "M",
-       Student "B" "F",
-       Student "C" "F",
-       Student "D" "M",
-       Student "E" "M",
-       Student "F" "F",
-       Student "G" "F",
-       Student "H" "F",
-       Student "I" "M"]
+students :: [Student]
+students = [Student "A" "M",
+           Student "B" "F",
+           Student "C" "F",
+           Student "D" "M",
+           Student "E" "M",
+           Student "F" "F",
+           Student "G" "F",
+           Student "H" "F",
+           Student "I" "M"]
 
-get :: Student -> Bool
-get (Student _ gender) = gender == "M"
+filterGender :: Student -> Bool
+filterGender (Student _ gender) = gender == "M"
 
 q2 :: IO ()
-q2 = putStrLn $ show $ filter get xxx
+q2 = putStrLn $ show $ filter filterGender students
