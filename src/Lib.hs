95d28a30e01229115c00e79138f5d0cb425d9554
Siew Li Meng <siewlimeng7@gmail.com>
2018-10-28 18:44:00 +0100

    Q6

--- src/Lib.hs
+++ src/Lib.hs
@@ -7,6 +7,7 @@ import Q2
 import Q3
 import Q4
 import Q5
+import Q6
 
 someFunc :: IO ()
 someFunc = do q1
@@ -14,3 +15,4 @@ someFunc = do q1
               q3
               q4
               q5
+              q6
